191dae86436d80fed3bd6f59246c96bbaa1b3bc2
Uduru <kurasiki.homura@gmail.com>
2021-05-18 00:18:05 +0800

    Added register function, post req need implementation

--- js/login.js
+++ js/login.js
@@ -5,10 +5,52 @@ $(document).ready(function() {
             account: $('#login input[name="username"]').val(),
             password: $('#login input[name="pwd"]').val()
         }, (data) => {
-            if(data == 'jump')
+            if (data == 'jump')
                 window.location.href = '../html/index.html';
-            else 
-                $('#ajax-output').html(`<br><br><font size="6"><b>${data}</b></font>`); // 顯示錯誤訊息的地方，帳密不正確之類的
+            else {
+                let warning = $("#signin-output");
+                let warning_text = $("#signin-output span");
+                warning_text.text(data);
+                warning.removeClass("fade-in").addClass("fade-in");
+            }
         })
     })
+
+    $("#login button#sign-up").on("click", function(event) {
+        event.preventDefault();
+        let login_page = $("form#login");
+        let signup_page = $("form#register");
+        login_page.css("display", "none");
+        signup_page.css("display", "flex");
+    });
+
+    $("#register #su-submit").on("click", function(event) {
+        event.preventDefault();
+        $.post("./register", {
+            username: $("fieldset#su-username input").val(),
+            password: $("fieldset#su-password input").val(),
+            email: $("fieldset#su-email input").val(),
+            lastname: $("fieldset#su-lastname input").val(),
+            firstname: $("fieldset#su-firstname input").val()
+        }, (data) => {
+            console.log(data);
+            let login_page = $("form#login");
+            let signup_page = $("form#register");
+            login_page.css("display", "flex");
+            signup_page.css("display", "none");
+        });
+    });
+
+    let reg_pwd_input = [$("#register #su-password input"), $("#register #su-password-confirm input")];
+    reg_pwd_input.forEach(function(e) {
+        e.on("input", function(event) {
+            if (reg_pwd_input[0].val() == reg_pwd_input[1].val()) {
+                console.log("same");
+                reg_pwd_input[1].css("border", "none");
+            } else {
+                console.log("not same");
+                reg_pwd_input[1].css("border", "red 5px solid");
+            }
+        });
+    });
 });
